dc88daa034c56a64c781905b4ce8620c01333011
aboba0amogus <invalid0user0intelligence@gmail.com>
2025-11-19 09:10:44 +1000

    поправил

--- zadanie5/tester_vm.tf
+++ zadanie5/tester_vm.tf
@@ -3,14 +3,15 @@ resource "libvirt_domain" "tester_domain"{
     vcpu = 4
     memory = 4096
     running = false
-    boot_devices = "cdrom","hdd","network"
+    boot_device {
+        dev = [ "cdrom","hd", "network"]
+    }
     network_interface {
         network_id     = libvirt_network.terraform_network.id
         hostname       = "tester"
         addresses      = ["10.100.100.10"]
     }
     disk{
-        device = "cdrom"
         file = "/home/astralinux.ru/akhristoforov/iso/installation-1.8.2.8-06.05.25_11.44.iso"
     }
 }
