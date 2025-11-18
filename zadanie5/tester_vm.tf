ede3b54dd225608057dd5b69d27dd4ceaeefa54e
aboba0amogus <invalid0user0intelligence@gmail.com>
2025-11-19 08:27:21 +1000

    aboba

--- zadanie5/tester_vm.tf
+++ zadanie5/tester_vm.tf
@@ -3,12 +3,14 @@ resource "libvirt_domain" "tester_domain"{
     vcpu = 4
     memory = 4096
     running = false
+    boot_devices = "cdrom","hdd","network"
     network_interface {
         network_id     = libvirt_network.terraform_network.id
         hostname       = "tester"
         addresses      = ["10.100.100.10"]
     }
     disk{
+        device = "cdrom"
         file = "/home/astralinux.ru/akhristoforov/iso/installation-1.8.2.8-06.05.25_11.44.iso"
     }
 }
